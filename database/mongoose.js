6a2a24f1070b8c396d6f78bf8bc340f5290dc6be
Taylor <taylorshephard1@gmail.com>
2022-06-29 23:36:11 -0500

    get and post recipe ratings

--- database/mongoose.js
+++ database/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/recipes", {
+mongoose.connect("mongodb://localhost:27017/recipe-ratings", {
   useNewUrlParser: true,
 });
 
@@ -11,3 +11,49 @@ db.once("open", function () {
   // we're connected!
   console.log("connected to database");
 });
+
+const recipeRatingSchema = {
+  recipe_name: String,
+  rating: Number,
+};
+
+const Rating = mongoose.model("recipe-ratings", recipeRatingSchema);
+
+const save = function (rating, callback) {
+  Rating.findOne({ recipe_name: rating.recipe_name }, function (error, data) {
+    if (error) {
+      console.log(error);
+    } else if (!error && data) {
+      callback("Rating already exists");
+    } else {
+      const newRating = new Rating({
+        recipe_name: rating.recipe_name,
+        rating: rating.rating,
+      });
+
+      newRating.save(function (error, ratingInfo) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log("rating: " + ratingInfo.recipe_name + " saved!");
+          callback(ratingInfo);
+        }
+      });
+    }
+  });
+};
+
+const find = function (callback) {
+  Rating.find({}, function (err, rating) {
+    if (err) {
+      console.log(errr);
+    } else {
+      callback(rating);
+    }
+  });
+};
+
+module.exports = {
+  save,
+  find,
+};
--- server/index.js
+++ server/index.js
@@ -1,9 +1,29 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const db = require("../database/mongoose");
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.json());
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+// get recipe ratings
+app.get("/recipe-ratings", (req, res) => {
+  var rating;
+  db.find(function (data) {
+    rating = data;
+    res.send(rating);
+  });
+});
+
+// post rating
+app.post("/recipe-ratings", (req, res) => {
+  var rating = req.body;
+  db.save(rating, function (data) {
+    res.send(data);
+  });
 });
 
 app.listen(port, () => {
