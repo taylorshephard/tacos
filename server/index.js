7ba80cb525981c89352bae7da73e97b7626e95e1
Taylor <taylorshephard1@gmail.com>
2022-07-05 20:41:35 -0500

    react client

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const db = require("../database/mongoose");
 const app = express();
-const port = 3000;
+const port = 8080;
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "../client")));
