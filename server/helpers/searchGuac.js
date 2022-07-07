b33f87c4ecfe167d5c7abb2d9302a99be5b71809
Taylor <taylorshephard1@gmail.com>
2022-07-06 20:12:22 -0500

    make excludeIngredients work

--- server/helpers/searchGuac.js
+++ server/helpers/searchGuac.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 
-let searchGuac = (searchTerm, callback) => {
+function searchGuac(searchTerm, callback) {
   const options = {
     method: "GET",
     url: "https://guac-is-extra.herokuapp.com/",
@@ -12,17 +12,37 @@ let searchGuac = (searchTerm, callback) => {
     params: {
       name: searchTerm.name,
       includeIngredients: searchTerm.includeIngredients,
-      excludeIngredients: searchTerm.excludeIngredients,
     },
   };
 
   axios(options)
     .then((response) => {
-      callback(response.data);
+      const data = excludeIngredients(
+        searchTerm.excludeIngredients,
+        response.data
+      );
+      callback(data);
+      console.log(response.data.length, data.length);
     })
     .catch((error) => {
       console.log(error);
     });
-};
+}
+
+// API excludeIngredients not working, This is a function to make sure that the ingredients that are excluded are not included in the results
+function excludeIngredients(excludeIngredientsList, ingredients) {
+  return ingredients.filter((recipe) => {
+    for (let i = 0; i < recipe.ingredients.length; i++) {
+      const ingredientName = recipe.ingredients[i].name;
+      if (
+        excludeIngredientsList &&
+        excludeIngredientsList.split(",").includes(ingredientName)
+      ) {
+        return false;
+      }
+    }
+    return true;
+  });
+}
 
 module.exports = { searchGuac };
